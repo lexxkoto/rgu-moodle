65385a6245f237420387e89485c4971d121a4eaa
Alex Walker <a.walker43@rgu.ac.uk>
2022-09-10 03:28:22 +0100

    MOO-2306: login page

--- theme/garthdee/layout/login.php
+++ theme/garthdee/layout/login.php
@@ -32,5 +32,7 @@ $templatecontext = [
     'bodyattributes' => $bodyattributes
 ];
 
-echo $OUTPUT->render_from_template('theme_boost/login', $templatecontext);
+$PAGE->requires->js_call_amd('theme_garthdee/loginpage', 'init', array());
+
+echo $OUTPUT->render_from_template('theme_garthdee/login', $templatecontext);
 
