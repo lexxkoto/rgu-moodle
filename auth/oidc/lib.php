0d6ff105bbe04adbde7db10fbd54dbb4b0078a79
Alex Walker <a.walker43@rgu.ac.uk>
2023-05-26 08:05:43 +0100

    VLE-4: added auth_oidc

--- auth/oidc/lib.php
+++ auth/oidc/lib.php
@@ -258,6 +258,7 @@ function auth_oidc_get_remote_fields() {
             'officeLocation' => get_string('settings_fieldmap_field_officeLocation', 'auth_oidc'),
             'preferredName' => get_string('settings_fieldmap_field_preferredName', 'auth_oidc'),
             'manager' => get_string('settings_fieldmap_field_manager', 'auth_oidc'),
+            'manager_email' => get_string('settings_fieldmap_field_manager_email', 'auth_oidc'),
             'teams' => get_string('settings_fieldmap_field_teams', 'auth_oidc'),
             'groups' => get_string('settings_fieldmap_field_groups', 'auth_oidc'),
             'roles' => get_string('settings_fieldmap_field_roles', 'auth_oidc'),
@@ -557,8 +558,7 @@ function auth_oidc_config_name_in_form(string $stringid) {
  */
 function auth_oidc_is_setup_complete() {
     $pluginconfig = get_config('auth_oidc');
-    if (empty($pluginconfig->clientid) || empty($pluginconfig->idptype) || empty($pluginconfig->clientauthmethod) ||
-        empty($pluginconfig->tenantnameorguid)) {
+    if (empty($pluginconfig->clientid) || empty($pluginconfig->idptype) || empty($pluginconfig->clientauthmethod)) {
         return false;
     }
 
