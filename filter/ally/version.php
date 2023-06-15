cf462099978c612685a1bc8f673a7d33ce805287
Alex Walker <a.walker43@rgu.ac.uk>
2023-06-15 19:02:39 +0100

    VLE-6: filter_ally

--- filter/ally/version.php
+++ filter/ally/version.php
@@ -26,11 +26,11 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'filter_ally';
-$plugin->release   = '4.0.5';
-$plugin->version   = 2022122100;
+$plugin->release   = '4.0.7';
+$plugin->version   = 2023041100;
 $plugin->requires  = 2022041900;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = [
-    'tool_ally'      => 2022122100,
-    'report_allylti' => 2022122100,
+    'tool_ally'      => 2023041100,
+    'report_allylti' => 2023041100,
 ];
