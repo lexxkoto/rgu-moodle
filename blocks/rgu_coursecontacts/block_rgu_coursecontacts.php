8d390ee6fcac3870d1c62432e803f6008bec9078
Alex Walker <a.walker43@rgu.ac.uk>
2023-06-15 02:37:56 +0100

    VLE-6: final version of block_rgu_coursecontacts

--- blocks/rgu_coursecontacts/block_rgu_coursecontacts.php
+++ blocks/rgu_coursecontacts/block_rgu_coursecontacts.php
@@ -12,6 +12,13 @@ class block_rgu_coursecontacts extends block_base {
         if ($this->content !== null) {
             return $this->content;
         }
+        
+        $roles = Array(
+            '10' => 'Module Coordinator',
+            '20' => 'Tutor',
+            '30' => 'Course Admin',
+            '40' => 'eLearning Support'
+        );
 
         $this->content = new stdClass();
         $this->content->footer = '';
@@ -28,7 +35,8 @@ class block_rgu_coursecontacts extends block_base {
                     $iAmShown = true;
                 }
                 $userDetails = $DB->get_record('user', Array('id'=>$user->userid));
-                $this->content->text .= '<div class="mb-2">'.$OUTPUT->user_picture($userDetails, Array('includefullname' => true)).'</div>';
+                $profileLink = new moodle_url('/user/view.php', array('id'=>$user->userid,'course'=>$COURSE->id));
+                $this->content->text .= '<div class="media course-contact-user mb-3"><span class="media-left mr-2">'.$OUTPUT->user_picture($userDetails, Array('includefullname' => false, 'size' => '48', 'alttext'=>'Picture of '.$userDetails->firstname.' '.$userDetails->lastname)).'</span><span class="media-body"><a href="'.$profileLink->out().'"><span class="name">'.$userDetails->firstname.' '.$userDetails->lastname.'</span><span class="role">'.$roles[$user->role].'</span></a></span></div>';
             }
             
             $context = context_course::instance($COURSE->id);
@@ -38,7 +46,14 @@ class block_rgu_coursecontacts extends block_base {
                     $this->content->text .= '<a class="btn btn-outline-danger btn-block mt-4" href="'.$hideLink->out().'"><i class="fa fa-minus-circle mr-1"></i> Remove My Details</a>';
                 } else {
                     $showLink = new moodle_url('/blocks/rgu_coursecontacts/show.php', Array('id'=>$COURSE->id));
-                    $this->content->text .= '<a class="btn btn-outline-success btn-block mt-4" href="'.$showLink->out().'"><i class="fa fa-plus-circle mr-1"></i> Show My Details</a>';
+                    $this->content->text .= '<div class="dropdown"><button class="btn btn-outline-success btn-block mt-4 dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"><i class="fa fa-plus-circle mr-1"></i> Show My Details</button><div class="dropdown-menu">';
+                    
+                    foreach($roles as $id=>$name) {
+                        $showLink = new moodle_url('/blocks/rgu_coursecontacts/show.php', Array('id'=>$COURSE->id,'role'=>$id));
+                        $this->content->text .= '<a class="dropdown-item" href="'.$showLink->out().'">Show me as '.$name.'</a>';
+                    }
+                    
+                    $this->content->text .= '</div></div>';
                 }
             }
         }
--- blocks/rgu_coursecontacts/show.php
+++ blocks/rgu_coursecontacts/show.php
@@ -3,6 +3,7 @@
     require_once('../../config.php');
     
     $course = required_param('id', PARAM_RAW);
+    $role = required_param('role', PARAM_RAW);
     $context = context_course::instance($course);
     
     require_login();
@@ -11,6 +12,7 @@
     $record = new stdClass();
     $record->courseid = $course;
     $record->userid = $USER->id;
+    $record->role = $role;
     
     $DB->insert_record('block_rgucc_users', $record);
     
