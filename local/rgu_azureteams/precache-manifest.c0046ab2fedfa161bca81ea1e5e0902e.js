8b92734abeddfecbff6175f4d23e5d4bf9fbd66d
Alex Walker <a.walker43@rgu.ac.uk>
2023-07-03 18:22:24 +0100

    VLE-6: local_azureteams

---
+++ local/rgu_azureteams/precache-manifest.c0046ab2fedfa161bca81ea1e5e0902e.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84437a64a1031968c9f9baad27d998a",
+    "url": "/local/rgu_teams/index.html"
+  },
+  {
+    "revision": "38a4cbc0c49e90ed9e0a",
+    "url": "/local/rgu_teams/static/css/main.a6141aa1.chunk.css"
+  },
+  {
+    "revision": "1915856b156df34a52fd",
+    "url": "/local/rgu_teams/static/js/0.bad7ae7b.chunk.js"
+  },
+  {
+    "revision": "7ab279f0ca99ce728f72",
+    "url": "/local/rgu_teams/static/js/10.c70ffc49.chunk.js"
+  },
+  {
+    "revision": "0efed989b0b65015c9ad",
+    "url": "/local/rgu_teams/static/js/11.1dc44156.chunk.js"
+  },
+  {
+    "revision": "b47922587df30603f3a3",
+    "url": "/local/rgu_teams/static/js/12.f229d3c3.chunk.js"
+  },
+  {
+    "revision": "376068f1a4793f256bc4",
+    "url": "/local/rgu_teams/static/js/13.a6270f1e.chunk.js"
+  },
+  {
+    "revision": "8cd07b3c01c787a15120",
+    "url": "/local/rgu_teams/static/js/14.eed83990.chunk.js"
+  },
+  {
+    "revision": "4393ad08a4ee2dcb0aa8",
+    "url": "/local/rgu_teams/static/js/15.a86a1c57.chunk.js"
+  },
+  {
+    "revision": "7d6ea7cda82d3ad57c2e",
+    "url": "/local/rgu_teams/static/js/16.2b1105a7.chunk.js"
+  },
+  {
+    "revision": "cbfce99bd139887a0cca",
+    "url": "/local/rgu_teams/static/js/17.0ab3ae2a.chunk.js"
+  },
+  {
+    "revision": "36b3e9ecbe21348afe7b",
+    "url": "/local/rgu_teams/static/js/18.39694569.chunk.js"
+  },
+  {
+    "revision": "5a088712d190c0529e3f",
+    "url": "/local/rgu_teams/static/js/19.36cd498d.chunk.js"
+  },
+  {
+    "revision": "431c7b0438ea580d418d",
+    "url": "/local/rgu_teams/static/js/20.50767873.chunk.js"
+  },
+  {
+    "revision": "779ebf2dd26ef9173275",
+    "url": "/local/rgu_teams/static/js/21.30477dc0.chunk.js"
+  },
+  {
+    "revision": "dbe4f770d510f720b3b1",
+    "url": "/local/rgu_teams/static/js/22.168436c4.chunk.js"
+  },
+  {
+    "revision": "c6c0d0d1448a891c526d",
+    "url": "/local/rgu_teams/static/js/23.365e233a.chunk.js"
+  },
+  {
+    "revision": "6e6cb4d8d074a4d67aff",
+    "url": "/local/rgu_teams/static/js/24.bec27ed6.chunk.js"
+  },
+  {
+    "revision": "7c4c56ff00b29fb12a01",
+    "url": "/local/rgu_teams/static/js/25.429f5b5f.chunk.js"
+  },
+  {
+    "revision": "b8c572a9065de8121188",
+    "url": "/local/rgu_teams/static/js/26.a1b2faaf.chunk.js"
+  },
+  {
+    "revision": "c103a9e39261edb7e6d4",
+    "url": "/local/rgu_teams/static/js/27.3ef1a5d3.chunk.js"
+  },
+  {
+    "revision": "c2556ff6f02fd0a8a29c",
+    "url": "/local/rgu_teams/static/js/28.e4f73105.chunk.js"
+  },
+  {
+    "revision": "83ae0aed6c60b6cd17dd",
+    "url": "/local/rgu_teams/static/js/29.ef9572ac.chunk.js"
+  },
+  {
+    "revision": "c8f1cff92bd33aba136d",
+    "url": "/local/rgu_teams/static/js/3.9294d5fd.chunk.js"
+  },
+  {
+    "revision": "8a1f4797478472cede47862237230d40",
+    "url": "/local/rgu_teams/static/js/3.9294d5fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb85c7a3b280f70d8c10",
+    "url": "/local/rgu_teams/static/js/30.542cb51a.chunk.js"
+  },
+  {
+    "revision": "1b802a7cce1b65077124",
+    "url": "/local/rgu_teams/static/js/31.45ffbfc6.chunk.js"
+  },
+  {
+    "revision": "9df8f6d93352d699104d",
+    "url": "/local/rgu_teams/static/js/32.d47a5e65.chunk.js"
+  },
+  {
+    "revision": "634bd9873bd190e2f601",
+    "url": "/local/rgu_teams/static/js/33.2c2cda07.chunk.js"
+  },
+  {
+    "revision": "396c6275a8f54cf57d6b",
+    "url": "/local/rgu_teams/static/js/4.4618ebae.chunk.js"
+  },
+  {
+    "revision": "96d9684c2a61c801f920",
+    "url": "/local/rgu_teams/static/js/5.4588b33a.chunk.js"
+  },
+  {
+    "revision": "743156bed3b22f48a0be",
+    "url": "/local/rgu_teams/static/js/6.aeaedb3e.chunk.js"
+  },
+  {
+    "revision": "e693c7cfca685e1cbb2c",
+    "url": "/local/rgu_teams/static/js/7.ea8c1f50.chunk.js"
+  },
+  {
+    "revision": "8d28288ddedc6cb407b8",
+    "url": "/local/rgu_teams/static/js/8.c9793e9e.chunk.js"
+  },
+  {
+    "revision": "561f6bd45c5339c56ac2",
+    "url": "/local/rgu_teams/static/js/9.dd7e2f74.chunk.js"
+  },
+  {
+    "revision": "38a4cbc0c49e90ed9e0a",
+    "url": "/local/rgu_teams/static/js/main.273acfd2.chunk.js"
+  },
+  {
+    "revision": "31357943237eabf4fdbe",
+    "url": "/local/rgu_teams/static/js/runtime-main.22e47380.js"
+  },
+  {
+    "revision": "e1f946d52a28fe4bef07a8a6e1e1c671",
+    "url": "/local/rgu_teams/static/media/calendar.e1f946d5.svg"
+  },
+  {
+    "revision": "b4d08b2516be723387a49ef892d8aa21",
+    "url": "/local/rgu_teams/static/media/genericError.b4d08b25.svg"
+  },
+  {
+    "revision": "17dc774881dde1c79b63aabfadf72c9c",
+    "url": "/local/rgu_teams/static/media/signin.17dc7748.svg"
+  }
+]);
